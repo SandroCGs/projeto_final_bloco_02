e8a548f9d4b4f605bda665cebc8fa4d11a9a2780
Sandro <sandrolp41@hotmail.com>
2025-12-17 12:03:15 -0300

    feat: implementa crud categoria

---
+++ src/categoria/entities/categoria.entity.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 100, nullable: false })
+  nome: string;
+
+  @IsNotEmpty()
+  @Column({ length: 1000, nullable: false })
+  descricao: string;
+
+  @Column({ default: true })
+  ativo: boolean;
+}
